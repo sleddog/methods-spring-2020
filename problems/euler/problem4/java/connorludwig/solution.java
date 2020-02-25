ba16da1d357fc16d252509dbb7463c935443c571
Connor Ludwig <connorlud@gmail.com>
2020-02-25 16:33:47 -0700

    Took logic out of  main method

--- problems/euler/problem4/java/connorludwig/solution.java
+++ problems/euler/problem4/java/connorludwig/solution.java
@@ -1,27 +1,34 @@
 public class Solution {
 
   public static void main(String[] args) {
-      int max = 0;
+      solve;
+  }
 
-      //multipy all number from 100 to 999 together
-      for (int i = 999; i >= 100; i--) {
-          for (int j = 999; j >= 100; j--) {
-              int p = i * j;
-              if (max < p && isPalindrome(p)) //passes product from loop into isPalindrome function.
-              {
-                  max = p; //if is a palindrome and greater than the last palindrome, set new max
-              }
+
+
+public static void solve(){
+  int max = 0;
+
+  //multipy all number from 100 to 999 together
+  for (int i = 999; i >= 100; i--) {
+      for (int j = 999; j >= 100; j--) {
+          int p = i * j;
+          if (max < p && isPalindrome(p)) //passes product from loop into isPalindrome function.
+          {
+              max = p; //if is a palindrome and greater than the last palindrome, set new max
           }
       }
-
-      if(max ==0){
-          System.out.println("No palindrome found");
-      }
-
-      else{
-          System.out.println(max);
-      }
   }
+
+  if(max ==0){
+      System.out.println("No palindrome found");
+  }
+
+  else{
+      System.out.println(max);
+  }
+}
+
 
   public static boolean isPalindrome(int input) {
       int rev = 0; // will hold the reversed number
