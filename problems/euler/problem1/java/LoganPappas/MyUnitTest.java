b9814547e1ed1815ce0b447ca8eafa6dd6ce2ff1
Logan Pappas <lpappas98@yahoo.com>
2020-03-03 16:04:13 -0700

    Added in test class

---
+++ problems/euler/problem1/java/LoganPappas/MyUnitTest.java
@@ -0,0 +1,15 @@
+
+
+import org.junit.Test;
+import static org.junit.Assert;
+import Driver.java
+
+public class UnitTest{
+    @Test
+    public void testProblem(){
+        MyUnit myUnit = new MyUnit();
+        int result = myUnit.solution(10);
+        assertEquals(23,result)
+    }
+
+} 
