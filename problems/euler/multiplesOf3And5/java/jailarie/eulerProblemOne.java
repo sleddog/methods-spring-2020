11ae8175eaf70f2972da161616187ffa09b84ed3
jailarie <jailarie.winter@gmail.com>
2020-03-03 09:31:31 -0700

    added user input to pick calculated number
    from @cjyovich suggestion on the code review.

--- problems/euler/multiplesOf3And5/java/jailarie/eulerProblemOne.java
+++ problems/euler/multiplesOf3And5/java/jailarie/eulerProblemOne.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
+//Jamie Winter
+
+//Solution to Euler problem one, multiples of 3 and 5.
+//https://projecteuler.net/problem=1
 
 public class eulerProblemOne {
 	public static void main(String[] args) {
-		int n = 1000;
+		System.out.println("Please enter a positive number: ");
+		Scanner consoleScanner = new Scanner(System.in);
+		int n = consoleScanner.nextInt();
 		int current = 0;
 		int sum = 0;
 		
