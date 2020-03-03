a3853e67eda506553dfa14011a70011ed3b8ebbd
Logan Pappas <lpappas98@yahoo.com>
2020-03-03 16:31:33 -0700

    Added in comments

--- problems/euler/problem1/java/LoganPappas/Driver.java
+++ problems/euler/problem1/java/LoganPappas/Driver.java
@@ -12,12 +12,12 @@ public class Driver {
      */
 
     public static void main(String[] args) {
-
+              //Make an int that will store the solution with an input of 1000 then print it
               int totalSum = solution(1000);
               System.out.println("The total sum of all multiples of 3 & 5 is " + totalSum);
 
 	}
-
+    //Method that takes in an int and then determines the multiples of 3 & 5 and adds them all together to whatever the input is
     public static int solution(int test_number){
         int totalSum=0;
         for(int i=0;i<test_number;i++){
