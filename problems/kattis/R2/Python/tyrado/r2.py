71e1a8587f946dbce47d57643ae32dfc094d487c
Tysen Radovich <radovich.tysen@gmail.com>
2020-03-03 15:54:35 -0700

    I cant add my unit testing

---
+++ problems/kattis/R2/Python/tyrado/r2.py
@@ -0,0 +1,19 @@
+def r2():
+    i=(int( input("put in your numbers for i: ")))
+    j=(int(input("put in number for j: ")))
+
+    print("This is I's value",i , "This is J's value",j)
+    
+    if i <= 1000 and i>= -1000 and j>=-1000 and j<=1000:
+        k =  0 
+        k=((j*2)-i)
+        print("k's value",k);
+        #j= (i+k)/2
+     
+    else:
+        pass 
+    #return(k)
+    #print(k)
+     
+            
+r2()
