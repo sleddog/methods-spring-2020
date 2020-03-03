394a6671e0434890f54ccb8c25b2c2a28f407d8e
Logan Pappas <lpappas98@yahoo.com>
2020-03-03 16:32:17 -0700

    Added in comments

--- problems/euler/problem1/java/LoganPappas/Driver.java
+++ problems/euler/problem1/java/LoganPappas/Driver.java
@@ -5,26 +5,27 @@
  */
 
 
-public class Driver {
+ public class Driver {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     /**
+      * @param args the command line arguments
+      */
 
-              int totalSum = solution(1000);
-              System.out.println("The total sum of all multiples of 3 & 5 is " + totalSum);
+     public static void main(String[] args) {
+               //Make an int that will store the solution with an input of 1000 then print it
+               int totalSum = solution(1000);
+               System.out.println("The total sum of all multiples of 3 & 5 is " + totalSum);
 
-	}
+ 	}
+     //Method that takes in an int and then determines the multiples of 3 & 5 and adds them all together to whatever the input is
+     public static int solution(int test_number){
+         int totalSum=0;
+         for(int i=0;i<test_number;i++){
+             if(i%3 == 0| i%5 == 0){
+                 totalSum+=i;
+             }
+         }
+         return totalSum;
+     }
 
-    public static int solution(int test_number){
-        int totalSum=0;
-        for(int i=0;i<test_number;i++){
-            if(i%3 == 0| i%5 == 0){
-                totalSum+=i;
-            }
-        }
-        return totalSum;
-    }
-
-}
+ }
