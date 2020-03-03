5915ced5715709ed99c8270210ead10f687a9c6b
Courtney Yovich <cjyovich@gmail.com>
2020-03-03 15:40:41 -0700

    Added unit tests to faktor.cpp

--- problems/kattis/faktor/C++/cjyovich/faktor.cpp
+++ problems/kattis/faktor/C++/cjyovich/faktor.cpp
@@ -4,10 +4,17 @@
 // Courtney Yovich, CSCI 494 - Industry Methods - Assignment 2 + 3
 
 #include <iostream>
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
 
 int citationsNeeded(int articles, int impact) {
 	int numNeeded = (articles * (impact - 1)) + 1;
 	return numNeeded;
+}
+
+TEST_CASE("Number of Articles and Impact Factor", "Reviewers Needed") {
+	REQUIRE(citationsNeeded(38, 24) == 875);
+	REQUIRE(citationsNeeded(1, 100) == 100);
 }
 
 int main() {
