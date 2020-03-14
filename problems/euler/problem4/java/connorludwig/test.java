6b94fa0ef2e53f2d4cf056c9b43fc78ae9b942cf
Connor Ludwig <connorlud@gmail.com>
2020-03-13 18:33:31 -0600

    Fixed test

---
+++ problems/euler/problem4/java/connorludwig/test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert;
+import Solution.java;
+
+
+//test for the palindrome method. See if it is returning true.
+public class Test{
+    @Test
+    public void testPalindrome(){
+        result = Solution.isPalindrome(2332)
+        assertTrue(result);
+    }
+
+}
